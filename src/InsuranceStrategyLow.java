7c9a5a23e9e194fa0a75fa608ca27dc82592991d
Michael Keen <mjkeen@bsu.edu>
2021-01-23 18:29:44 -0500

    Implementing the abstract class

---
+++ src/InsuranceStrategyLow.java
@@ -0,0 +1,17 @@
+public class InsuranceStrategyLow extends InsuranceStrategy {
+
+    @Override
+    public int getConstant() {
+        return 0;
+    }
+
+    @Override
+    public double getWeight() {
+        return 0.365;
+    }
+
+    @Override
+    public int getAdjustment() {
+        return 0;
+    }
+}
