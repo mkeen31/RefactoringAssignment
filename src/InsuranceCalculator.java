173f10ff814b9183fc9b6a2260445d2fb2982b36
Michael Keen <mjkeen@bsu.edu>
2021-01-23 17:34:28 -0500

    Analyzing the sample application

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -8,7 +8,7 @@ public class InsuranceCalculator {
         } else if (income <= 60000) {
             return (income-30000)*0.1+76500;
         } else {
-            return (income-60000)*0.02+105600;
+            return (income - 60000) * 0.02 + 105600;
         }
     }
 
